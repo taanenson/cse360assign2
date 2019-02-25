91b724d5206e5ad421fcdb02373961b89a2e5b6c
taane <taane@DESKTOP-E816M0A>
2019-02-24 18:59:21 -0700

    Saving the original file given by the teacher with only javadoc code added

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,80 @@
+
+package cse360assign3;
+/**
+* <h1>Calculator</h1>
+* This class will be a simple calculator with the variable total and functions 
+* getTotal, add, subtract, multiply, divide, and getHistory. All functions are 
+* currently empty.
+*
+* @author  Tyler Aanenson
+* @version 1.0
+* @since   2019-02-24
+*/
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	   * This method will be used to return the current value of total, 
+	   * it currently does nothing.
+	   * @param Unused
+	   * @return 0 (this will later be changed to total)
+	   */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	   * This method will be used to add a number to the current total, 
+	   * it currently does nothing.
+	   * @param value This is the number which will be added to total
+	   * @return Nothing
+	   */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	   * This method will be used to subtract a number to the current total, 
+	   * it currently does nothing.
+	   * @param value This is the number which will be subtracted from the total.
+	   * @return Nothing
+	   */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	   * This method will be used to multiply a number by the current total, 
+	   * it currently does nothing.
+	   * @param value This is the number which will be multiplied with the total.
+	   * @return Nothing
+	   */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	   * This method will be used to divide the current total by a number,  
+	   * it currently does nothing.
+	   * @param value This is the number which the total will be divided by
+	   * @return Nothing
+	   */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	   * This method will print the history of commands entered, 
+	   * it currently does nothing.
+	   * @param Unused
+	   * @return Nothing (an empty string)
+	   */
+	public String getHistory () {
+		return "";
+	}
+}
