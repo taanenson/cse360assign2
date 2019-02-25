3ba3b7be79fe6f3b1eccc5f216d6bb7ca121cf32
taane <taane@DESKTOP-E816M0A>
2019-02-24 20:10:21 -0700

    Added functionality to add, subtract, multiply, and divide methods. Also updated javadoc documentation

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,13 +1,18 @@
 
-package cse360assign3;
+package cse360assign2;
 /**
 * <h1>Calculator</h1>
 * This class will be a simple calculator with the variable total and functions 
-* getTotal, add, subtract, multiply, divide, and getHistory. All functions are 
-* currently empty.
-*
+* getTotal, add, subtract, multiply, divide, and getHistory. getTotal() returns 
+* the current value of total, add() adds the passed value to the total and 
+* replaces total with the sum. subtract() takes the difference of total and 
+* the passed value and replaces total with the difference. multiply() multiplies 
+* total by the passed value and replaces total with the product. divide() 
+* divides the total by the passed value and replaces total with thte value of 
+* the division. getHistory() does nothing.
+* 
 * @author  Tyler Aanenson
-* @version 1.0
+* @version 2.0
 * @since   2019-02-24
 */
 public class Calculator {
@@ -25,47 +30,43 @@ public class Calculator {
 	   * @return 0 (this will later be changed to total)
 	   */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
-	   * This method will be used to add a number to the current total, 
-	   * it currently does nothing.
-	   * @param value This is the number which will be added to total
+	   * This method adds a number to the current total.
+	   * @param value This is the integer to be added to total
 	   * @return Nothing
 	   */
 	public void add (int value) {
-		
+		total += value;
 	}
 	
 	/**
-	   * This method will be used to subtract a number to the current total, 
-	   * it currently does nothing.
-	   * @param value This is the number which will be subtracted from the total.
+	   * This method subtracts a number from the current total.
+	   * @param value This is the number to be subtracted from the total.
 	   * @return Nothing
 	   */
 	public void subtract (int value) {
-		
+		total -= value;
 	}
 	
 	/**
-	   * This method will be used to multiply a number by the current total, 
-	   * it currently does nothing.
-	   * @param value This is the number which will be multiplied with the total.
+	   * This method multiplies the current total by a number.
+	   * @param value This is the number to be multiplied with the total.
 	   * @return Nothing
 	   */
 	public void multiply (int value) {
-		
+		total *= value;
 	}
 	
 	/**
-	   * This method will be used to divide the current total by a number,  
-	   * it currently does nothing.
-	   * @param value This is the number which the total will be divided by
+	   * This method divides the current total by a number.
+	   * @param value This is the number the total will be divided by
 	   * @return Nothing
 	   */
 	public void divide (int value) {
-		
+		total /= value;
 	}
 	
 	/**
